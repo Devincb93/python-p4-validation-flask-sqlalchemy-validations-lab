5d620a1109b43bc0b3130506edd2d6f4a53ed741
Devin <devincb93@gmail.com>
2024-07-24 18:46:18 -0700

    complete

--- server/models.py
+++ server/models.py
@@ -11,7 +11,22 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name) <= 0:
+            raise ValueError("Please enter a valid new Author")
+        if db.session.query(Author).filter(Author.name == name).first():
+            raise ValueError("Name already exists in database")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        if not phone_number.isdigit():
+            raise ValueError("Phone number must only be digits")
+        return phone_number
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +42,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('content') 
+    def validates_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Not enough characters please add more")
+        return content
+        
 
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Maximum amount of characters(250) reached")
+        return summary
+
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_titles = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_titles):
+            raise ValueError("Title must contain an approved title beginner")
+        return title
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        categories = ["Fiction", "Non-Fiction"]
+        if not any(type in category for type in categories):
+            raise ValueError("Category must be one from the approved list")
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
